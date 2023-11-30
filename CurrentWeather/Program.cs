a8425bec3b75156a797a1f8b77e2852dff6578d2
Eric Garner <ericgarner81@gmail.com>
2023-11-29 22:28:51 -0600

    Add project files.

---
+++ CurrentWeather/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrentWeather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ CurrentWeather/Weather.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace CurrentWeather
+{
+    public class Weather
+    {
+        public string GetZip()
+        {
+            var apiKeyObj = File.ReadAllText("appsettings.json");
+
+            var apiKey = JsonNode.Parse(apiKeyObj).GetValue("apiKey").ToString();
+
+            Console.WriteLine("Enter Zip Code:");
+
+            var zip = Console.ReadLine();
+
+            return zip;
+
+        }
+
+
+
+
+
+        }
+}
