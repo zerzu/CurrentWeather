0247109c664de4b4d6b8623e349c68b273647839
Eric Garner <ericgarner81@gmail.com>
2023-11-29 23:52:17 -0600

    completed exercise

--- CurrentWeather/Program.cs
+++ CurrentWeather/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            Weather weather = new();
+
+            weather.GetWeather();
+
+           
         }
     }
 }
--- CurrentWeather/Weather.cs
+++ CurrentWeather/Weather.cs
@@ -4,26 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace CurrentWeather
 {
     public class Weather
     {
-        public string GetZip()
+        public void GetWeather()
         {
+            // gets appsettings.json file and reads in all text
+
             var apiKeyObj = File.ReadAllText("appsettings.json");
 
-            var apiKey = JsonNode.Parse(apiKeyObj).GetValue("apiKey").ToString();
+            // gets the value pair for apiKey from appsettings.json and returns it as a string of useable text
 
-            Console.WriteLine("Enter Zip Code:");
+            var apiKey = JObject.Parse(apiKeyObj).GetValue("apiKey").ToString();
+
+            Console.Write("Enter Zip Code: ");
 
             var zip = Console.ReadLine();
+            
+            var url = $"https://api.openweathermap.org/data/2.5/weather?zip={zip},us&appid={apiKey}&units=imperial";
 
-            return zip;
+            var _client = new HttpClient();
 
+            var jText = _client.GetStringAsync(url).Result;
+
+            var weatherObjMain = JObject.Parse(jText)["main"];
+
+            Console.WriteLine($"Temp: {weatherObjMain["temp"]}");
+            Console.WriteLine($"--------------------------------");
+            Console.WriteLine($"Feels Like: {weatherObjMain["feels_like"]}");
         }
-
-
 
 
 
